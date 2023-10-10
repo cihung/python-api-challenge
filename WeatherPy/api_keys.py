249236bee0e22641d74a50827e04858bf474d91b
cihung <cindyhungling@gmailcom>
2023-10-10 00:37:33 -0400

    upload images

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8bc152e15a385fb655fb6e155690bf9b"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "11379298e8f54e5083eeb446a8fd2026"
